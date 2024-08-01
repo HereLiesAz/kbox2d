cdae0c97621401a789097a24a8d832be21823684
Josef Friedrich <josef@friedrich.rocks>
2024-08-01 22:23:13 +0200

    Improve taglet

--- jbox2d-testbed/src/main/java/de/pirckheimer_gymnasium/jbox2d/testbed/framework/GitHubTaglet.java
+++ jbox2d-testbed/src/main/java/de/pirckheimer_gymnasium/jbox2d/testbed/framework/GitHubTaglet.java
@@ -2,7 +2,7 @@ package de.pirckheimer_gymnasium.jbox2d.testbed.framework;
 
 import com.sun.source.doctree.DocTree;
 import jdk.javadoc.doclet.Taglet;
-
+import com.sun.source.doctree.UnknownBlockTagTree;
 import javax.lang.model.element.Element;
 import java.util.List;
 import java.util.Set;
@@ -10,6 +10,20 @@ import java.util.Set;
 public class GitHubTaglet implements Taglet
 {
     private static final String NAME = "box2d";
+
+    protected String genLink(String text)
+    {
+        String relativePath = text;
+        String display = text;
+        int firstSpace = text.indexOf(' ');
+        if (firstSpace != -1)
+        {
+            relativePath = text.substring(0, firstSpace);
+            display = text.substring(firstSpace).trim();
+        }
+        return String.format("<a href='%s%s'>%s</a>", "", relativePath,
+                display);
+    }
 
     @Override
     public String getName()
@@ -32,6 +46,18 @@ public class GitHubTaglet implements Taglet
     @Override
     public String toString(List<? extends DocTree> tags, Element element)
     {
-        return "sb.toString()";
+        if (tags.isEmpty())
+        {
+            return null;
+        }
+        StringBuilder buf = new StringBuilder(String
+                .format("<dl><dt><span class=\"strong\">%s</span></dt>", ""));
+        for (DocTree tag : tags)
+        {
+            String text = ((UnknownBlockTagTree) tag).getContent().get(0)
+                    .toString();
+            buf.append("<dd>").append(genLink(text)).append("</dd>");
+        }
+        return buf.toString();
     }
 }
